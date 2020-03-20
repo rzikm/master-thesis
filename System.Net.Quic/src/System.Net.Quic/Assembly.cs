f23a6593915aefb91db6ca683bd5b79b8d151e6a
Radek Zikmund <r.zikmund.rz@gmail.com>
2020-03-20 10:51:26 +0100

    Moved Functional tests to separate project

--- System.Net.Quic/src/System.Net.Quic/Assembly.cs
+++ System.Net.Quic/src/System.Net.Quic/Assembly.cs
@@ -1,1 +1,2 @@
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("System.Net.Quic.Unit.Tests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("System.Net.Quic.Functional.Tests")]
